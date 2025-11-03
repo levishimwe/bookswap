fc389c4bf7db8d92e90520f29140b0c402dbfff5
levishimwe <i.levis@alustudent.com>
2025-11-03 14:43:43 +0200

    first commit

---
+++ android/build.gradle.kts
@@ -0,0 +1,11 @@
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.bookswap"
+        minSdk = 23
+        targetSdk = 34
+    }
+
+    ndkVersion = "27.0.12077973"
+}
